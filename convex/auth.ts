f03a478e98af116dcbb45f6c64d40e3323712645
Vijit Dhingra <vijit@uselark.io>
2025-12-14 15:35:54 -0800

    create + subscribe customer

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,21 @@
 import { convexAuth } from "@convex-dev/auth/server";
 import { Password } from "@convex-dev/auth/providers/Password";
+import { internal } from "./_generated/api";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [Password],
+  callbacks: {
+    afterUserCreatedOrUpdated: async (context, args) => {
+      console.log("afterUserCreatedOrUpdated", args);
+
+      context.scheduler.runAfter(
+        0,
+        internal.lark.potentiallyCreateCustomerAndSubscribeToFreePlan,
+        {
+          email: args.profile.email,
+          user_id: args.userId,
+        }
+      );
+    },
+  },
 });
